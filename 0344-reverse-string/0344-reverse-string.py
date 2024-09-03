5047425ebb4d9ceac59cdd68bef4e8e2acb21b92
olit2004 <olit2950@gmail.com>
2024-09-03 16:46:23 +0300

    Time: 144 ms (8.87%), Space: 19.5 MB (6.14%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,4 +4,7 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        n= len (s)-1 
+        for i in range(len(s)//2) :
+                s[i],s[n]=s[n],s[i]
+                n-=1
